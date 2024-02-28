049defa8fe78cd9f6f63d3c5aa50fa4b2225537e
Tushar <tushard3998@gmail.com>
2024-02-28 13:38:38 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,7 @@
+console.log('Hello World');
+
+// Get the current date and time
+const now = new Date();
+
+console.log('Current Date:', now.toDateString()); // Output: Current Date: Tue Feb 27 2024
+console.log('Current Time:', now.toTimeString().split(' ')[0]);
